364203134540e364639296f02792e67c71db82cd
Guillermo Duque <guille980627@gmail.com>
2023-09-16 23:56:21 -0500

    02-template-string

---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,12 @@
+console.log("Hello world")
+
+// TEMPLATE SRING
+
+const nombre = 'Guillermo',
+      apellido = 'Duque';  
+
+// const fullName = nombre + ' ' + apellido;
+const fullName = `${ nombre } ${ apellido }`
+// BOTH WAYS ARE VALID
+
+console.log( fullName );
